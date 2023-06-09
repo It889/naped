0e051b998bc4f589332086d20a25f6002321852e
Italo <Italonexen@gmail.com>
2023-06-09 01:02:04 -0300

    feat: Initial project setup and configuration

---
+++ src/Components/Header.vue
@@ -0,0 +1,21 @@
+<template>
+    <header>
+        <ul>
+            <li>Inicio</li>
+            <li>Séries</li>
+            <li>Filmes</li>
+            <li>Animes</li>
+            <li>Jogos</li>
+        </ul>
+    </header>
+</template>
+
+<script>
+export default {
+    name: 'Header'
+}
+</script>
+
+<style scoped>
+
+</style>
